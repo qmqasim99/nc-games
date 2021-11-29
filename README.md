95ef40933f51b1674c2719dfaea48d82c280d684
qmqasim99 <Qmqasim99@gmail.com>
2021-11-29 14:17:28 +0000

    Complete DB Seeding

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Your database will be PSQL, and you will interact with it using [node-postgres](
 
 You will need to create _two_ `.env` files for your project: `.env.test` and `.env.development`. Into each, add `PGDATABASE=<database_name_here>`, with the correct database name for that environment (see `/db/setup.sql` for the database names). Double check that these `.env` files are .gitignored.
 
-You have also been provided with a `db` folder with some data, a [setup.sql](./db/setup.sql) file and  a `seeds` folder. You should also take a minute to familiarise yourself with the npm scripts you have been provided.
+You have also been provided with a `db` folder with some data, a [setup.sql](./db/setup.sql) file and a `seeds` folder. You should also take a minute to familiarise yourself with the npm scripts you have been provided.
 
 The job of `index.js` in each the data folders is to export out all the data from that folder, currently stored in separate files. This is so that, when you need access to the data elsewhere, you can write one convenient require statement - to the index file, rather than having to require each file individually. Think of it like a index of a book - a place to refer to! Make sure the index file exports an object with values of the data from that folder with the keys:
 
@@ -37,8 +37,8 @@ Each category should have:
 Each user should have:
 
 - `username` which is the primary key & unique
-- `avatar_url`
 - `name`
+- `avatar_url`
 
 Each review should have:
 
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,146 @@
+const format = require("pg-format");
+const db = require("../connection");
+
 const seed = (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
+
   // 1. create tables
-  // 2. insert data
+
+  return (
+    db
+      .query("DROP TABLE if EXISTS comments")
+
+      .then(() => {
+        return db.query("DROP TABLE if EXISTS reviews");
+      })
+      .then(() => {
+        return db.query("DROP TABLE if EXISTS categories");
+      })
+      .then(() => {
+        return db.query("DROP TABLE if EXISTS users");
+      })
+      .then(() => {
+        return db.query(`create table categories (
+          slug varchar(100) primary key not null,
+          description text
+        )`);
+      })
+      .then(() => {
+        return db.query(`create table users (
+        username varchar(50) primary key not null,
+        name varchar(100) not null,
+        avatar_url text
+      )`);
+      })
+      .then(() => {
+        return db.query(`create table reviews (
+        review_id SERIAL PRIMARY KEY, 
+        title varchar not null,
+        review_body text,
+        designer varchar,
+        review_img_url text default 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+        votes INT DEFAULT 0,
+        category varchar(100) references CATEGORIES (slug) ON DELETE CASCADE,
+        owner varchar(50) references users(username) ON DELETE CASCADE,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW() 
+      )`);
+      })
+      .then(() => {
+        return db.query(`create table comments (
+        comment_id SERIAL PRIMARY KEY, 
+        author varchar(50) references users(username) ON DELETE CASCADE,
+        review_id INT references REVIEWS (review_id) ON DELETE CASCADE,
+        votes INT DEFAULT 0,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW() ,
+        body text
+      )`);
+      })
+
+      // 2. insert data
+      .then(() => {
+        // format categories into array
+        const formattedCategories = categoryData.map((category) => {
+          return [category.slug, category.description];
+        });
+
+        const queryStr = format(
+          `insert into categories (slug, description) VALUES %L returning *;`,
+          formattedCategories
+        );
+
+        return db.query(queryStr);
+      })
+      .then(() => {
+        // format users into array
+        const formattedUsers = userData.map((user) => {
+          return [user.username, user.name, user.avatar_url];
+        });
+
+        const queryStr = format(
+          `insert into users (username, name, avatar_url) VALUES %L returning *;`,
+          formattedUsers
+        );
+
+        return db.query(queryStr);
+      })
+      .then(() => {
+        // format reviews into array
+        const formattedReviews = reviewData.map((review) => {
+          return [
+            review.title,
+            review.review_body,
+            review.designer,
+            review.review_img_url,
+            review.votes,
+            review.category,
+            review.owner,
+            review.created_at,
+          ];
+        });
+
+        const queryStr = format(
+          `insert into reviews (
+            title ,
+        review_body ,
+        designer ,
+        review_img_url ,
+        votes,
+        category,
+        owner,
+        created_at ) VALUES %L returning *;`,
+          formattedReviews
+        );
+
+        return db.query(queryStr);
+      })
+      .then(() => {
+        // format comments into array
+        const formattedComments = commentData.map((comment) => {
+          return [
+            comment.author,
+            comment.review_id,
+            comment.votes,
+            comment.created_at,
+            comment.body,
+          ];
+        });
+
+        const queryStr = format(
+          `insert into comments (
+            author ,
+            review_id ,
+            votes ,
+            created_at ,
+            body) VALUES %L returning *;`,
+          formattedComments
+        );
+
+        return db.query(queryStr);
+      })
+      .then((response) => {
+        console.log(response.rows);
+      })
+  );
 };
 
 module.exports = seed;
